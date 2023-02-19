bbfc027c9dd21340f9503bf94e789037096eb18a
Taimur-s <94656455+Taimur-s@users.noreply.github.com>
2023-02-20 01:04:45 +0500

    commit

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import  express  from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+app.use('/posts', postRoutes);
 app.use(bodyParser.json({limit: "30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
 app.use(cors());
@@ -11,6 +13,6 @@ app.use(cors());
 const CONNECTION_URL='mongodb+srv://MERNstack-socialapp:0900786@cluster0.lshb5qv.mongodb.net/?retryWrites=true&w=majority';
 const PORT= process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
-    .then(()=> app.listen(PORT,()=> console.log('Server Running on Port :$ { PORT }')))
+    .then(()=> app.listen(PORT,()=> console.log("Server Running on Port: ${PORT}")))
     .catch((error)=> console.log(error.message));
-    mongoose.set('strictQuery',false);  
+mongoose.set('strictQuery',false);  
